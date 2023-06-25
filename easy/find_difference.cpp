e199887ee6663373662fece0e32aac690feec823
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-06-25 11:48:54 -0600

    Create find_difference.cpp

---
+++ easy/find_difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int i = 0;
+        for(i; i < s.size(); i++){
+            if(s[i] != t[i]){
+                return t[i];
+            }
+        }
+
+        return t[i];
+
+    }
+};
